6e38b0cf779ddee20535a750943c672acc74c7c3
Chukwuemeka Maduekwe <chukwuemeka@viewcrunch.com>
2022-01-14 14:06:29 +0100

    updated source

--- react/source/theme.js
+++ react/source/theme.js
@@ -11,64 +11,9 @@ const theme = createTheme({
     },
     secondary: {
       main: "rgb(141, 202, 141)",
-      // contrastText: "#fff",
     },
     spacing: 24,
   },
 });
 
-const globalTheme = createTheme(
-  {
-    components: {
-      MuiPaper: {
-        styleOverrides: {
-          root: {
-            overflow: "hidden",
-            margin: "10px auto",
-            boxSizing: "border-box",
-            // padding: 10,
-          },
-          rounded: {
-            borderRadius: 5,
-          },
-        },
-      },
-
-      MuiTable: {
-        styleOverrides: {
-          root: {
-            minWidth: 300,
-          },
-        },
-      },
-      MuiTableCell: {
-        styleOverrides: {
-          head: {
-            backgroundColor: theme.palette.common.black,
-            color: theme.palette.common.white,
-            cursor: "pointer",
-          },
-          body: {
-            fontSize: 14,
-          },
-        },
-      },
-      MuiTableRow: {
-        styleOverrides: {
-          root: {
-            "&:nth-of-type(odd)": {
-              backgroundColor: theme.palette.action.hover,
-            },
-            // hide last border
-            "&:last-child td, &:last-child th": {
-              border: 0,
-            },
-          },
-        },
-      },
-    },
-  },
-  theme
-);
-
-export default globalTheme;
+export default theme;
